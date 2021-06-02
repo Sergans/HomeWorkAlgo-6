0c5372ee75563f45de7be068af3ec2b6c1e3963c
Сергей <gan7er@yandex.ru>
2021-06-02 18:58:24 +0300

    Начало

---
+++ Task-6-1/Task-6-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_6_1
+{
+    public class Node
+    {
+        public int value { get; set; }
+        public List<Edge> Edges = new List<Edge>();//Исходящие
+    }
+    public class Edge
+    {
+        public int Weight { get; set; }
+        public Node Node { get; set; }
+
+
+    }
+    public class Graph
+    {
+        public List<Node> nodes = new List<Node>();
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
